9c733832db7a4d68364a45f69c40b63133481f78
pedrovidal <peu_vs@yahoo.com.br>
2018-05-23 18:19:36 -0300

    Add lcs to utils.py

--- utils.py
+++ utils.py
@@ -1,8 +1,21 @@
 import csv
-
+import numpy as np
 
 def read_lattes_csv(f, remove_header=True):
   data = list(csv.reader(f, delimiter=';'))
   if remove_header:
     data = data[1:]
   return data
+
+def lcs(a, b):
+    size_a = len(a)
+    size_b = len(b)
+    dp = np.zeros((size_a + 1, size_b + 1), dtype = int)
+    longest = 0
+    for i in range(1, size_a + 1):
+        for j in range(1, size_b + 1):
+            if a[i - 1] == b [ j -1]:
+                dp[i][j] = dp[i - 1][j - 1] + 1
+                if dp[i][j] > longest:
+                    longest = dp[i][j]
+    return longest
