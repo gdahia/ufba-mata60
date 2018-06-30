636e1094b064c832daf7381ec632adf3b880d036
Gabriel Dahia <gdahia@gmail.com>
2018-06-30 17:22:19 -0300

    Fix #8, remove 'Colaboracoes' column

--- preprocess.py
+++ preprocess.py
@@ -221,7 +221,7 @@ def main():
         # update filtered degrees
         n_collab[u - 1] += w
         n_collab[v - 1] += w
-  data['Colaboracoes'] = n_collab[data.index.values]
+
   collab = pd.DataFrame(edges)
   print('Done.')
 
