4487dbed3eb22ad8561dc6220700609fd6f91a38
Gabriel Dahia <gdahia@gmail.com>
2018-07-03 11:47:52 -0300

    Add negative examples generation

--- train.py
+++ train.py
@@ -5,21 +5,57 @@ FLAGS = None
 
 
 def train(cvs, collabs, binary=True):
-  # generate examples from valid edge
+  # generate positive examples
   instances = []
   labels = []
+  edges = []
   for _, (u, v, w) in collabs.iterrows():
     # generate instances
-    u = cvs.loc[[u]]
-    v = cvs.loc[[v]]
-    instance1 = np.squeeze(np.concatenate([u, v], axis=1))
-    instance2 = np.squeeze(np.concatenate([v, u], axis=1))
+    u_row = cvs.loc[[u]]
+    v_row = cvs.loc[[v]]
+    instance1 = np.squeeze(np.concatenate([u_row, v_row], axis=1))
+    instance2 = np.squeeze(np.concatenate([v_row, u_row], axis=1))
     instances.append(instance1)
     instances.append(instance2)
 
-    # generate labels
+    # generate corresponding labels
     labels.append(w)
     labels.append(w)
+
+    # add edge to edges list
+    edges.append((u, v))
+    edges.append((v, u))
+
+  # make queries fast to answer
+  edges = set(edges)
+
+  # generate negative examples
+  n_neg_examples = len(instances) // 2
+  for _ in range(n_neg_examples):
+    # randomly choose non edge
+    while True:
+      # pick two random endpoints
+      u, v = np.random.choice(len(cvs), 2, replace=False)
+
+      # recover 'non_edge' endpoints' indices
+      u_index = cvs.index.values[u]
+      v_index = cvs.index.values[v]
+
+      # check if it really is non edge
+      if (u_index, v_index) not in edges:
+        # generate negative instance
+        u_row = cvs.loc[[u_index]]
+        v_row = cvs.loc[[v_index]]
+        instance1 = np.squeeze(np.concatenate([u_row, v_row], axis=1))
+        instance2 = np.squeeze(np.concatenate([v_row, u_row], axis=1))
+        instances.append(instance1)
+        instances.append(instance2)
+
+        # generate corresponding labels
+        labels.append(0)
+        labels.append(0)
+
+        break
 
 
 def main():
